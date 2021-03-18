960696f6045971bb9a70667b05f5361843245591
Josh <josharms00@gmail.com>
2021-03-18 15:45:51 -0400

    Adding project to git.

---
+++ build/barrel.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(barrel)
+
+//======================================================================
+//
+//	barrel, 32x32@4, 
+//	+ palette 16 entries, not compressed
+//	+ 16 tiles Metatiled by 4x4 not compressed
+//	Total size: 32 + 512 = 544
+//
+//	Time-stamp: 2021-03-18, 15:41:57
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_BARREL_H
+#define GRIT_BARREL_H
+
+#define barrelTilesLen 512
+extern const unsigned int barrelTiles[128];
+
+#define barrelPalLen 32
+extern const unsigned short barrelPal[16];
+
+#endif // GRIT_BARREL_H
+
+//}}BLOCK(barrel)
---
+++ build/laser.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(laser)
+
+//======================================================================
+//
+//	laser, 8x8@4, 
+//	+ palette 256 entries, not compressed
+//	+ 1 tiles not compressed
+//	Total size: 512 + 32 = 544
+//
+//	Time-stamp: 2021-03-18, 15:41:58
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_LASER_H
+#define GRIT_LASER_H
+
+#define laserTilesLen 32
+extern const unsigned int laserTiles[8];
+
+#define laserPalLen 512
+extern const unsigned short laserPal[256];
+
+#endif // GRIT_LASER_H
+
+//}}BLOCK(laser)
